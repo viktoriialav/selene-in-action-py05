257c5581651d547716450f0c274c9a7c45eaa698
Viktoriia Lavrova <viktoriia.lav2908@gmail.com>
2024-06-27 19:28:02 +0300

    Study some new features and add in test files

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from selene import browser
+from selene import browser as selene_browser, Browser, Config
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromService
 from webdriver_manager.chrome import ChromeDriverManager
@@ -8,24 +8,47 @@ from webdriver_manager.chrome import ChromeDriverManager
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
     # browser.config.base_url = 'https://todomvc.com/examples/emberjs'
-    browser.config.base_url = 'https://todomvc-emberjs-app.autotest.how'
-    browser.config.timeout = 2.0
+    selene_browser.config.base_url = 'https://todomvc-emberjs-app.autotest.how'
+    selene_browser.config.timeout = 2.0
+    selene_browser.config.type_by_js = True
     driver_options = webdriver.ChromeOptions()
-    driver_options.add_argument('--headless')
-    browser.config.driver_options = driver_options
+    # driver_options.add_argument('--headless')
+    selene_browser.config.driver_options = driver_options
     # Create your own driver for browser
-    browser.config.driver = webdriver.Chrome(
+    selene_browser.config.driver = webdriver.Chrome(
         service=ChromService(executable_path=ChromeDriverManager().install()),
         options=driver_options,
     )
 
-    # # for Yandex browser
-    # yandex_driver_file = r'F:\Drivers\yandexdriver.exe'
-    # browser.config.driver = webdriver.Chrome(
-    #     service=ChromService(executable_path=yandex_driver_file),
-    #     options=driver_options,
-    # )
+    '''
+    # for Yandex browser
+    yandex_driver_file = r'F:\Drivers\yandexdriver.exe'
+    browser.config.driver = webdriver.Chrome(
+        service=ChromService(executable_path=yandex_driver_file),
+        options=driver_options,
+    )
+    '''
 
     yield
 
-    browser.quit()
+    selene_browser.quit()
+
+
+@pytest.fixture()
+def driver():
+    driver_options = webdriver.ChromeOptions()
+    driver_options.add_argument('--headless')
+    driver = webdriver.Chrome(
+        service=ChromService(executable_path=ChromeDriverManager().install()),
+        options=driver_options,
+    )
+
+    yield driver
+
+    driver.quit()
+
+
+@pytest.fixture()
+def browser(driver):
+
+    yield Browser(Config(driver=driver))
--- tests/test_todos_operations.py
+++ tests/test_todos_operations.py
@@ -1,13 +1,74 @@
-from selene import browser, have, be
+from selene import browser as selene_browser, have, be, query, command
 
 
 def test_complete_todo():
-    browser.open('/')
-    browser.element('#new-todo').should(be.blank)
+    selene_browser.open('/')
+    selene_browser.element('#new-todo').should(be.blank)
 
-    browser.element('#new-todo').type('a').press_enter()
-    browser.element('#new-todo').type('b').press_enter()
-    browser.element('#new-todo').type('c').press_enter()
-    browser.all('#todo-list>li').with_(
-        timeout=browser.config.timeout * 2,
-    ).should(have.size(3))
+    selene_browser.element('#new-todo').type('a').press_enter()
+    # selene_browser.element('#new-todo').perform(command.select_all)
+    selene_browser.element('#new-todo').type('b').press_enter()
+    selene_browser.element('#new-todo').type('c').press_enter()
+    # selene_browser.all('#todo-list>li').with_(
+    #     timeout=selene_browser.config.timeout * 2,
+    # ).should(have.size(3))
+
+    # selene_browser.all('#todo-list>li').should(have.size(3))
+    #
+    # selene_browser.all('#todo-list>li').first.should(have.exact_text('a'))
+    # selene_browser.all('#todo-list>li').second.should(have.exact_text('a'))
+    # selene_browser.all('#todo-list>li')[3].should(have.exact_text('a'))
+
+    selene_browser.all('#todo-list>li').should(have.exact_texts('a', 'b', 'c'))
+    # selene_browser.all('#todo-list>li').second.element('.toggle').click()
+    selene_browser.all('#todo-list>li').element_by(have.exact_text('b')).element('.toggle').click()
+    selene_browser.all('#todo-list>li').by(have.css_class('completed')).should(have.exact_texts('b'))
+    selene_browser.all('#todo-list>li').by(have.no.css_class('completed')).should(have.exact_texts('a','b'))
+
+
+
+
+    '''
+    # It's more about meeting some conditions
+    browser.all('#todo-list>li').should(have.size(3))
+    # It's more about waiting for something to be completed
+    browser.all('#todo-list>li').wait.for_(have.size(3))
+    '''
+
+    '''
+    # if we want to use selenium.webdriver. But this commands won't be effective for slow browsers.
+    # They can't wait in comparison with "should" in selene
+    # The first way -->
+    from selene import by
+    assert browser.driver.find_elements(*by.css('#todo-list>li')).__len__ == 3
+    # The second way
+    from selenium.webdriver.common.by import By
+    assert browser.driver.find_elements(By.CSS_SELECTOR, '#todo-list>li').__len__ == 3
+    # If we want let the 'selenium.webdriver' wait the request execution , we should use the following options:
+    from selenium.webdriver.support.wait import WebDriverWait
+    WebDriverWait(driver=browser.driver, timeout=3.0).until(
+        lambda driver: driver.find_elements(By.CSS_SELECTOR, '#todo-list>li').__len__
+        == 3
+    )
+    '''
+
+
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+
+
+def test_complete_todo_1(driver, browser):
+    driver.get('https://todomvc-emberjs-app.autotest.how')
+    assert (
+            driver.find_element(By.CSS_SELECTOR, '#new-todo').get_attribute('value') == ''
+    )
+
+    # the same in selene: assert browser.element('#new-todo').get(query.attribute('value')) == ''
+    # or: browser.element('#new-todo').should(have.attribute('value').value(''))
+    # or: browser.element('#new-todo').should(be.blank)
+
+    # browser.element('#new-todo').locate() is the same as : driver.find_element(By.CSS_SELECTOR, '#new-todo')
+
+    driver.find_element(By.CSS_SELECTOR, '#new-todo').send_keys('a' + Keys.ENTER)
+    driver.find_element(By.CSS_SELECTOR, '#new-todo').send_keys('a' + Keys.ENTER)
+    driver.find_element(By.CSS_SELECTOR, '#new-todo').send_keys('a' + Keys.ENTER)
